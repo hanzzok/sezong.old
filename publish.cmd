85fb6ffd6ab41685afa8b909fc19110efcd8c9ef
RanolP <public.ranolp@gmail.com>
2019-01-26 18:19:28 +0900

    build: write publish script

---
+++ publish.cmd
@@ -0,0 +1,19 @@
+@echo off
+if exist dist (
+  echo Stage 0: Remove dist folder
+  rmdir /S /Q dist & echo Stage: Remove Complete
+)
+echo Stage 1: Compile
+tsc & echo Stage 1: Compile Complete & (
+echo Stage 2: Copy files
+copy LICENSE dist > nul
+copy README.md dist > nul
+copy package.json dist > nul
+echo Stage 3: Upgrade version
+cd dist/
+npm version %1
+npm publish
+echo Stage 4: Synchronize
+copy /Y package.json .. 2>nul
+cd ../
+)
